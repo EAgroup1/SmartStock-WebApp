3c449bed5be8aa7278501c500ece802723b9060c
Carlos Méndez <c.mendez@tobeit.es>
2021-06-20 10:48:46 +0200

    mongo prod

--- backend/src/keys.ts
+++ backend/src/keys.ts
@@ -1,3 +1,3 @@
 export const mongodb = {
-    "URI": "mongodb://root:u65r5c5U@mongo:27017/SmartStockDB?authSource=admin"
+    "URI": "mongodb://root:u65r5c5U@10.5.0.6:27017/SmartStockDB?authSource=admin"
 };
